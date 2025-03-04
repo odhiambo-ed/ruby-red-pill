9b3df185b04c9d17768dcd54e1f3d619e38138a5
odhiambo-ed <odhiambo.edward111@gmail.com>
2025-03-04 20:28:12 +0300

    concat with shovel operator

--- strings/concatenation.rb
+++ strings/concatenation.rb
@@ -1,2 +1,7 @@
+# With plus operator
 greetings = "Glory " + "be " + "to " + "God"
 puts greetings
+
+# With shovel operator
+second_greetings = "All Glory " << "to " << "God"
+puts second_greetings
