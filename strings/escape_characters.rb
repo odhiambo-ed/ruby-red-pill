72f8fb73a3a3781d0770197eedc58c41cc4a44d4
odhiambo-ed <odhiambo.edward111@gmail.com>
2025-03-04 20:41:00 +0300

    add escape character

---
+++ strings/escape_characters.rb
@@ -0,0 +1,9 @@
+
+\\  #=> Need a backslash in your string?
+\b  #=> Backspace
+\r  #=> Carriage return, for those of you that love typewriters
+\n  #=> Newline. You'll likely use this one the most.
+\s  #=> Space
+\t  #=> Tab
+\"  #=> Double quotation mark
+\'  #=> Single quotation mark
